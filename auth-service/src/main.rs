782cd49d89c1019199e3cc8d607abb1e16b7e12b
Michael Jung <jung@mijn-design.de>
2024-05-15 08:20:25 +0200

    TODOs resolved

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -17,6 +17,6 @@ async fn main() {
 }
 
 async fn hello_handler() -> Html<&'static str> {
-    // TODO: Update this to a custom message!
-    Html("<h1>Hello, World!</h1>")
+    /// TODO: Update this to a custom message!
+    Html("<h1>Auth-Service ok!</h1>")
 }
